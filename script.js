100ee70ef0e518656969db117b7f7db7c769bad7
Ley Sampaio <wesleysampaio81@gmail.com>
2024-03-31 18:46:38 -0300

    update final

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-
-// Verifica se a largura da janela é menor que 800 pixels (um valor arbitrário para dispositivos móveis)
 if (window.innerWidth < 800) {
-  // Seleciona o contêiner das imagens
-  const gridImg = document.querySelector('.grid-img');
-  // Seleciona o contêiner da descrição
-  const descricao = document.querySelector('.descricao');
-  // Insere a descrição após o contêiner das imagens
+  const gridImg = document.querySelector(".grid-img");
+
+  const descricao = document.querySelector(".descricao");
+
   gridImg.parentNode.insertBefore(descricao, gridImg.nextSibling);
 }
