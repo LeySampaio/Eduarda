7578858f45e0044d10706eee7c0699d57a68712c
Ley Sampaio <wesleysampaio81@gmail.com>
2024-04-01 19:31:18 -0300

    uptade

--- scriptfotos.js
+++ scriptfotos.js
@@ -1,18 +1,18 @@
 function nextSlide() {
-  const carousel = document.getElementById('carousel');
-  const itemWidth = carousel.querySelector('li').offsetWidth;
+  const carousel = document.getElementById("carousel");
+  const itemWidth = carousel.querySelector("li").offsetWidth;
   const translateValue = -itemWidth;
 
-  carousel.style.transition = 'transform 0.5s ease';
-  carousel.style.transform = `translateX(${translateValue}px)`; 
+  carousel.style.transition = "transform 0.5s ease";
+  carousel.style.transform = `translateX(${translateValue}px)`;
 
   setTimeout(() => {
     carousel.appendChild(carousel.firstElementChild);
-    carousel.style.transition = 'none';
-    carousel.style.transform = 'translateX(0)';
+    carousel.style.transition = "none";
+    carousel.style.transform = "translateX(0)";
   }, 500);
 }
 
 if (window.matchMedia("(max-width: 800px)").matches) {
-  setInterval(nextSlide, 6000);
+  setInterval(nextSlide, 8000);
 }
