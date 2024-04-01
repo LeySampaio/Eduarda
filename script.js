ace821a1eaf47c7e67a667ee6e4c25c5d1f12827
Ley Sampaio <wesleysampaio81@gmail.com>
2024-04-01 12:41:17 -0300

    uptade slide mobile

--- script.js
+++ script.js
@@ -6,32 +6,6 @@ if (window.innerWidth < 800) {
   gridImg.parentNode.insertBefore(descricao, gridImg.nextSibling);
 }
 
-
-$(document).ready(function(){
-  const carousel = $('#carousel');
-  let translateValue = 0;
-
-  $('#nextBtn').click(function(){
-    const itemWidth = carousel.find('li').outerWidth(true);
-    const numItems = carousel.find('li').length;
-    const maxTranslate = -(itemWidth * (numItems - 1));
-  
-    if (translateValue > maxTranslate) {
-      translateValue -= itemWidth;
-      carousel.css('transform', `translateX(${translateValue}px)`);
-    }
-  });
-
-  $('#prevBtn').click(function(){
-    const itemWidth = carousel.find('li').outerWidth(true);
-    
-    if (translateValue < 0) {
-      translateValue += itemWidth;
-      carousel.css('transform', `translateX(${translateValue}px)`);
-    }
-  });
-});
-
 function nextSlide() {
   const carousel = document.getElementById('carousel');
   const itemWidth = carousel.querySelector('li').offsetWidth;
@@ -47,5 +21,28 @@ function nextSlide() {
     carousel.style.transform = 'translateX(0)';
    }, 500);
   }
-
   setInterval(nextSlide, 3000);
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.getElementById('carousel');
+    let translateValue = 0;
+  
+    function nextSlide() {
+      const itemWidth = carousel.querySelector('li').offsetWidth;
+      const numItems = carousel.children.length;
+      let translateValue = -itemWidth;
+  
+      carousel.style.transition = 'transform 0.5s ease';
+      carousel.style.transform = `translateX(${translateValue}px)`; 
+  
+      setTimeout(() => {
+        carousel.appendChild(carousel.firstElementChild);
+        carousel.style.transition = 'none';
+        carousel.style.transform = 'translateX(0)';
+      }, 500);
+    }
+  
+    if (window.matchMedia("(max-width: 800px)").matches) {
+      setInterval(nextSlide, 3000);
+    }
+  });
