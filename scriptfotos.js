2873a09cac786ea52af1d7840987f6b47ae12bd5
Ley Sampaio <wesleysampaio81@gmail.com>
2024-04-01 18:56:19 -0300

    uptade js

---
+++ scriptfotos.js
@@ -0,0 +1,18 @@
+function nextSlide() {
+  const carousel = document.getElementById('carousel');
+  const itemWidth = carousel.querySelector('li').offsetWidth;
+  const translateValue = -itemWidth;
+
+  carousel.style.transition = 'transform 0.5s ease';
+  carousel.style.transform = `translateX(${translateValue}px)`; 
+
+  setTimeout(() => {
+    carousel.appendChild(carousel.firstElementChild);
+    carousel.style.transition = 'none';
+    carousel.style.transform = 'translateX(0)';
+  }, 500);
+}
+
+if (window.matchMedia("(max-width: 800px)").matches) {
+  setInterval(nextSlide, 2000);
+}
